57be48f3f8bb282289cb83637eb1bf80fef76bfe
Edge <baiban1994@gmail.com>
2017-08-23 15:40:35 +0800

    之后开发商家页

--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -20,7 +20,7 @@
     ></ratingselect>
     <div class="rating-wrap">
       <ul>
-        <li v-for='rati in ratings' class="rati-item">
+        <li v-for='rati in ratings' class="rati-item" v-show='needShow(rati.rateType,rati.text)'>
           <div class="avatar-wrap">
             <img :src="rati.avatar" alt="" class='avatar'> 
           </div>
@@ -30,7 +30,7 @@
             <div class="star-wrap">
               <star :size='24' :score='rati.score'></star><span class="ne-time" v-if='rati.deliveryTime'>{{rati.deliveryTime}}分钟送达</span>
             </div>
-            <p class="desc">{{rati.text}}</p>
+            <p class="comment">{{rati.text}}</p>
             <div class="recommend-wrap" v-if='rati.recommend && rati.recommend.length'>
               <span :class="{'icon-thumb_up': rati.rateType === 0,'icon-thumb_down': rati.rateType === 1}"></span>
               <span v-for='recoitem in rati.recommend' class="re-item">{{recoitem}}</span>
@@ -64,7 +64,7 @@ export default{
   data(){
     return {
       selectType: ALL,
-      onlyContent: true,
+      onlyContent: false,
       ratings:[]
     }
   },
@@ -81,18 +81,28 @@ export default{
     }, (response) => {} );
   },
   methods:{
-    contentToggle1(){
+    needShow(type,text){
+      if(this.onlyContent && !text){
+        return false;
+      }
 
+      if(this.selectType === ALL){
+        return true;
+      }else{
+        return type === this.selectType;
+      }
     },
-    ratingtypeSelect1(){
-
+    ratingtypeSelect1(type){
+      this.selectType = type[0]
+    },
+    contentToggle1(onlyContent){
+      this.onlyContent = onlyContent[0]
     }
   },
   filters:{
     setTime(time){
-      time = new Date(time);
-      time = formatDate(time,"yyyy-MM-dd hh:mm");
-      return time
+      let date = new Date(time);
+      return formatDate(date,"yyyy-MM-dd hh:mm");
     }
   }
 }
@@ -201,33 +211,31 @@ export default{
               color: rgb(147,153,159)
               line-height:12px
               vertical-align: top
-          .desc
+          .comment
             font-size: 12px 
             color:rgb(7,17,27)
             line-height: 18px
           .recommend-wrap
             font-size:0px
             margin-top: 8px
-            overflow: hidden
             width: 100%
-            height: 18px
             [class^='icon']
               font-size: 12px
               line-height: 16px
+              margin-right: 8px
               &[class$='up']
                 color: rgb(0,160,220)
               &[class$='down']
                 color: rgb(183,187,191)
             .re-item
               display:inline-block
-              height: 16px
               padding: 0 6px
               font-size: 9px
               color: rgb(147,153,159)
               line-height: 16px
               border: 1px solid rgba(7,17,27,.1)
               border-radius: 2px
-              margin-left: 8px
+              margin:0 8px 4px 0
               vertical-align: top
 </style>
 
